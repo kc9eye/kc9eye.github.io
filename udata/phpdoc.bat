98f66613f708227a710914cc7eb5a40f83307e91
Paul W. Lane <kc9eye@outlook.com>
2018-10-29 13:08:35 -0500

    update to udata

--- udata/phpdoc.bat
+++ udata/phpdoc.bat
@@ -1,3 +1,3 @@
 REM Generate documents
 REM ignoring the controller direcotries and wwwroot
-php phpDocumentor.phar -d ..\etc\,..\lib,..\class,..\models -t .
+php phpDocumentor.phar -d ..\etc\,..\lib,..\framework,..\models -t .
